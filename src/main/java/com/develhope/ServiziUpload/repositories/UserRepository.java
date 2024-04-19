9751944b0e9d63553c68f086c7690e228ad5129a
Domenico Ribaudo <domenicoribaudo06@gmail.com>
2024-04-19 23:31:57 +0200

    Initial commit

---
+++ src/main/java/com/develhope/ServiziUpload/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.develhope.ServiziUpload.repositories;
+
+import com.develhope.ServiziUpload.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/test/java/com/develhope/ServiziUpload/ServiziUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.develhope.ServiziUpload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiziUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
